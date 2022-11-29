827f5c9f8a9e0241bde085526f96a3f8c6aace8a
JesusPereiraNet <jesuspereira.net@gmail.com>
2022-11-29 17:24:26 -0400

    Agregado el async

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
+const morgan = require("morgan");
+
+const PORT = process.env.PORT || 3000;
 
 //settings
-app.set('port', process.env.PORT || 3000);
-app.set('json spaces', 2);
+app.set("port", process.env.PORT || 3000);
+app.set("json spaces", 2);
 
 //midleware
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get("/", async (req, res) => {
+  await res.status(200).send({ Message: "OK" });
+});
+
 //routes
-app.use('/api/productos', require('./src/productos'));
+app.use("/api/productos", require("./src/productos"));
 
 //empezando el servidor
-app.listen(3000, () => {
-    console.log('Server on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server on port ${PORT}`);
 });
